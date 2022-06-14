75a88355c2818335d14cf0f0bd21d3d4344b59a9
LEESUNHANG2 <95415549+LEESUNHANG2@users.noreply.github.com>
2022-06-14 22:48:12 +0900

    Android 광고 배너 및 출시 준비
    광고 배너 셋팅과 출시에 필요한 로고,그래픽소스 이미지 생성,
    앱 아이콘 변경,
    액션바 제거

--- To-Do List/app/src/main/java/com/sunhang/to_dolist/adapter/TodoAdapter.kt
+++ To-Do List/app/src/main/java/com/sunhang/to_dolist/adapter/TodoAdapter.kt
@@ -9,8 +9,12 @@ import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.sunhang.to_dolist.database.TodoDatabase
 import com.sunhang.to_dolist.databinding.DialogEditBinding
 import com.sunhang.to_dolist.databinding.ListItemTodoBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import model.TodoInfo
 import java.text.SimpleDateFormat
 import java.util.*
@@ -19,7 +23,7 @@ import java.util.*
 class TodoAdapter:ListAdapter<TodoInfo,TodoAdapter.ToDoViewHolder>(diffUtil) {
 
     //private var TodoList:ArrayList<TodoInfo> = ArrayList()
-
+    private lateinit var roomDatabase: TodoDatabase
 
     //inner class로 생성하지 않으면 TodoAdapter에 접근할 수 없다.
    inner class ToDoViewHolder(private val binding: ListItemTodoBinding): RecyclerView.ViewHolder(binding.root) {
@@ -35,9 +39,15 @@ class TodoAdapter:ListAdapter<TodoInfo,TodoAdapter.ToDoViewHolder>(diffUtil) {
                     .setTitle("[주의]")
                     .setMessage("제거하시면 데이터는 복구되지 않습니다! \n정말 삭제하시겠습니까?")
                     .setPositiveButton("제거",DialogInterface.OnClickListener { dialogInterface, i ->
-                        val currentList = currentList.toMutableList()
-                        currentList.removeAt(position)
-                        submitList(currentList)
+                        //비동기 처리
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val currentList = currentList.toMutableList()
+                            currentList.removeAt(position)
+                            //delete database
+                            roomDatabase.todoDao().deleteTodoData(todoitem)
+                            submitList(currentList)
+
+                        }
                         Toast.makeText(binding.root.context,"제거되었습니다!",Toast.LENGTH_SHORT)
                     })
                     .setNegativeButton("취소",DialogInterface.OnClickListener { dialogInterface, i ->
@@ -48,17 +58,21 @@ class TodoAdapter:ListAdapter<TodoInfo,TodoAdapter.ToDoViewHolder>(diffUtil) {
 
             //리스트 수정 클릭 연동
             binding.root.setOnClickListener{
-                val currentList = currentList.toMutableList()
                 val bindingDialog = DialogEditBinding.inflate(LayoutInflater.from(binding.root.context),binding.root,false)
                 bindingDialog.etMemo.setText(todoitem.todoContent)
                 AlertDialog.Builder(binding.root.context)
                     .setTitle("To-Do 수정하기")
                     .setView(bindingDialog.root)
                     .setPositiveButton("작성 완료", DialogInterface.OnClickListener { dialogInterface, i ->
-                        todoitem.todoContent= bindingDialog.etMemo.text.toString()
-                        todoitem.todoDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
-                        currentList.set(adapterPosition,todoitem)
-                        submitList(currentList)
+                        CoroutineScope(Dispatchers.IO).launch {
+                            todoitem.todoContent = bindingDialog.etMemo.text.toString()
+                            todoitem.todoDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
+                            //update database
+                            roomDatabase.todoDao().updateTodoData(todoitem)
+                            submitList(roomDatabase.todoDao().getAllReadData() as ArrayList<TodoInfo>)
+                        }
+                        Toast.makeText(binding.root.context,"수정되었습니다!",Toast.LENGTH_SHORT)
+
                     })
                     .setNeutralButton("취소",DialogInterface.OnClickListener { dialogInterface, i ->
 
@@ -71,6 +85,8 @@ class TodoAdapter:ListAdapter<TodoInfo,TodoAdapter.ToDoViewHolder>(diffUtil) {
     //뷰 홀더가 생성됨.(각 리스트 아이템 1개가 생성 될 때마다 이 오버라이드 메소드가 호출됨)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoAdapter.ToDoViewHolder {
         val binding = ListItemTodoBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        // 룸 데이터베이스 초기화
+        roomDatabase = TodoDatabase.getInstance(binding.root.context)!!
         return ToDoViewHolder(binding)
     }
     //뷰 홀더가 바인딩(결합)이 이루어질 때 해줘야 할 처리들을 구현.
@@ -80,7 +96,7 @@ class TodoAdapter:ListAdapter<TodoInfo,TodoAdapter.ToDoViewHolder>(diffUtil) {
     companion object{
         val diffUtil=object:DiffUtil.ItemCallback<TodoInfo>(){
             override fun areItemsTheSame(oldItem: TodoInfo, newItem: TodoInfo): Boolean {
-                return oldItem.todoContent==newItem.todoContent
+                return oldItem.id == newItem.id
             }
 
             @SuppressLint("DiffUtilEquals")
@@ -91,11 +107,7 @@ class TodoAdapter:ListAdapter<TodoInfo,TodoAdapter.ToDoViewHolder>(diffUtil) {
 
         }
     }
-    fun delete(position: Int){
-        val currentList = getCurrentList().toMutableList()
-        currentList.removeAt(position)
-        submitList(currentList)
-    }
+
 
 
 
